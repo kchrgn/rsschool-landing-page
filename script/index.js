44a60e2cbc2f29b5ca3f3612c128efcfebf470e4
Andrey Kochergin <ASK@MacBook-Air-Daniil.local>
2026-09-25 22:59:47 +0300

    feat: implement burger menu

---
+++ script/index.js
@@ -0,0 +1,5 @@
+const primaryButton = document.getElementById('primary-button');
+
+primaryButton.addEventListener('click', () => {
+     location.href = 'catalog.html';
+} )
--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
 const toggleButton = document.getElementById('theme-toggle');
-const primaryButton = document.getElementById('primary-button');
 const body = document.body;
 
 if (localStorage.getItem('currentTheme') === 'dark') body.classList.add('dark');
@@ -13,7 +12,32 @@ toggleButton.addEventListener('click', () => {
     }
 })
 
-primaryButton.addEventListener('click', () => {
-     location.href = 'catalog.html';
-} )
+const burgerMenuButton = document.getElementById('burger-menu')
+const burgerMenuContent = document.getElementById('burger-menu-content')
 
+burgerMenuButton.addEventListener('click', () => {
+    burgerMenuButton.classList.toggle('coffee-burger-menu-close');
+    burgerMenuContent.classList.toggle('burger-menu-active');
+    body.classList.toggle('body-scroll-disable');
+})
+
+burgerMenuContent.addEventListener('click', (event) => {
+    console.log(event.target.tagName)
+    if (event.target.tagName === 'A') closeBurgerMenu();
+})
+
+function closeBurgerMenu () {
+    burgerMenuButton.classList.remove('coffee-burger-menu-close');
+    burgerMenuContent.classList.remove('burger-menu-active');
+    body.classList.remove('body-scroll-disable')
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        closeBurgerMenu();
+    }
+})
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') closeBurgerMenu();
+})
